b6769f3c93f0e5405c090ced18783536601d32b8
BEC Tutoring Staff <bectutoringstaff@Carloss-MacBook-Air.local>
2016-01-30 13:05:52 -0600

    Initial Commit

---
+++ connor's project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  connor's project
+//
+//  Created by BEC Tutoring Staff on 1/30/16.
+//  Copyright (c) 2016 BEC Tutoring Staff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
